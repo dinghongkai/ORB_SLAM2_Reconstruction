004fcc94380cf5671d0a1c24ad5c2da50d008eda
dinghongkai <hkding1112@163.com>
2020-08-12 19:23:46 +0800

    comments

---
+++ Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/DBoW2/BowVector.cpp" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/BowVector.cpp.o"
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/DBoW2/FORB.cpp" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/FORB.cpp.o"
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/DBoW2/FeatureVector.cpp" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/FeatureVector.cpp.o"
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/DBoW2/ScoringObject.cpp" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/ScoringObject.cpp.o"
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/DUtils/Random.cpp" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DUtils/Random.cpp.o"
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/DUtils/Timestamp.cpp" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DUtils/Timestamp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DBoW2_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/hongkai/project/cv/opencv-3.4.6/build"
+  "/home/hongkai/project/cv/opencv-3.4.6/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/include/opencv"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/core/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/flann/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/hdf/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/imgproc/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/ml/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/phase_unwrapping/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/photo/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/plot/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/reg/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/surface_matching/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/video/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/viz/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/xphoto/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/dnn/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/freetype/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/fuzzy/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/hfs/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/img_hash/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/imgcodecs/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/shape/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/videoio/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/highgui/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/superres/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/ts/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/bioinspired/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/dnn_objdetect/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/features2d/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/line_descriptor/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/saliency/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/text/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/calib3d/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/ccalib/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/datasets/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/objdetect/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/rgbd/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/stereo/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/structured_light/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/tracking/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/videostab/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/xfeatures2d/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/ximgproc/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/xobjdetect/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/aruco/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/bgsegm/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/dpm/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/face/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/optflow/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/sfm/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/stitching/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/stereo_kitti.dir/DependInfo.cmake
@@ -0,0 +1,100 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/Examples/Stereo/stereo_kitti.cc" "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/build/CMakeFiles/stereo_kitti.dir/Examples/Stereo/stereo_kitti.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "H5_BUILT_AS_DYNAMIC_LIB"
+  "qh_QHpointer"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkIOExport_AUTOINIT=2(vtkIOExportOpenGL2,vtkIOExportPDF)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/local/include/vtk-8.2"
+  "/usr/local/include/eigen3"
+  "/usr/include/eigen3"
+  "/usr/local/include/pcl-1.9"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/opt/ensenso/development/c/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/build"
+  "/home/hongkai/project/cv/opencv-3.4.6/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/include/opencv"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/core/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/flann/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/hdf/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/imgproc/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/ml/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/phase_unwrapping/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/photo/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/plot/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/reg/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/surface_matching/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/video/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/viz/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/xphoto/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/dnn/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/freetype/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/fuzzy/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/hfs/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/img_hash/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/imgcodecs/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/shape/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/videoio/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/highgui/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/superres/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/ts/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/bioinspired/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/dnn_objdetect/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/features2d/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/line_descriptor/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/saliency/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/text/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/calib3d/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/ccalib/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/datasets/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/objdetect/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/rgbd/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/stereo/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/structured_light/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/tracking/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/videostab/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/xfeatures2d/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/ximgproc/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/xobjdetect/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/aruco/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/bgsegm/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/dpm/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/face/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/optflow/include"
+  "/home/hongkai/project/cv/opencv_contrib-3.4.6/modules/sfm/include"
+  "/home/hongkai/project/cv/opencv-3.4.6/modules/stitching/include"
+  "/usr/local/include/vtk-8.2/vtkogg/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hongkai/disk/project/slam/ORB_SLAM2_Reconstruction/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
